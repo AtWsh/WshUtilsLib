b3094ed5b25072a3a0876db745f4ad836734c243
wenshenghui <wenshenghui@evergrande.cn>
2018-08-10 15:24:39 +0800

    注释

--- wshlibrary/src/main/java/it/wsh/cn/wshlibrary/proxy/ProxyUtils.java
+++ wshlibrary/src/main/java/it/wsh/cn/wshlibrary/proxy/ProxyUtils.java
@@ -3,6 +3,13 @@ package it.wsh.cn.wshlibrary.proxy;
 
 import java.lang.reflect.Proxy;
 
+/**
+ * 动态代理工具
+ * 1. 提供ProxyInterceptor接口，在实际操作执行执行的操作，返回值表示是否拦截
+ * 2. weakRef表示对object的缓存是否需要采用弱引用方式
+ * 3. postUI表示执行方法，是否需要在UI线程中执行
+ * 4. 操作示例: response = ProxyUtils.getUIProxy(response);  表示response的所有方法都执行在UI线程中
+ */
 public class ProxyUtils {
 
     public static <T> T getProxy(Object object, Class<?>[] intfs, ProxyInterceptor interceptor, boolean weakRef, boolean postUI) {
